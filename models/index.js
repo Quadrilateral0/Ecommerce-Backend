6d269b9a286d945ae6f65721aecef274af0da12b
Quadrilateral <amandacdulaney@gmail.com>
2022-04-18 17:49:05 -0400

    removed excess code

--- models/index.js
+++ models/index.js
@@ -11,29 +11,18 @@ Product.belongsTo(Category, {
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'category_id'
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  // Define the third table needed to store the foreign keys
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  // Define an alias for when data is retrieved
-  as: 'product_alias'
-});
+  through: ProductTag
+  });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  // Define the third table needed to store the foreign keys
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  // Define an alias for when data is retrieved
-  as: 'tag_alias'
+  through: ProductTag
 });
 
 module.exports = {
